2f18211ab09c4c9a309ef002f6ac04068ed47392
EugeniuZaharia <78173175+EugeniuZaharia@users.noreply.github.com>
2021-03-17 17:00:40 +0100

    full

--- name.js
+++ name.js
@@ -1,4 +1,8 @@
-const names: {
+const chalk = require("chalk");
 
+const log = console.log;
 
-}
+log(chalk.blue("Ospi"));
+log(chalk.red("Agnes"));
+log(chalk.white("Jessica"));
+log(chalk.yellow("Luca"));
